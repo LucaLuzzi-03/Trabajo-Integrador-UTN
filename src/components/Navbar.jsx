84ea34a38b222392eef24670774f127cb291631e
LucaLuzzi-03 <luzziluca6@gmail.com>
2023-09-08 10:15:17 -0300

    Estructura de carpetas definidas

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+export const Navbar = () => {
+  return (
+    <>
+      <h1>NAVBAR</h1>
+      <hr />
+    </>
+  )
+}
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,9 @@
+
+export const ProductsPage = () => {
+  return (
+    <>
+      <h1>Products page</h1>
+      <hr />
+    </>
+  )
+}
